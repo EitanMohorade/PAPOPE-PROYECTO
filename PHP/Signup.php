b11f2d62045ab65b925fdb70738ee96e918fe64d
Fede <fedelva8@gmail.com>
2021-11-08 15:36:07 -0300

    Modifique el sign up

--- PHP/Signup.php
+++ PHP/Signup.php
@@ -1,8 +1,16 @@
 <?php
 require_once "papopeconexion.php";
 include "menuPAPOPE.php";
-
-
-
-
 ?>
+<form method="POST">
+Usuario: <input type="text" name="us_reg">
+<br>
+Contraseña: <input type="password" name="pass_reg">
+Confirmar Contraseña: <input type="password" name="pass_reg_conf">
+
+
+
+
+</form>
+
+
