41ae7fc707264a1ee1e5da516c6cc0099b1d29cb
Fede <fedelva8@gmail.com>
2021-11-08 16:20:09 -0300

    modifique el signup

--- PHP/Signup.php
+++ PHP/Signup.php
@@ -3,14 +3,24 @@ require_once "papopeconexion.php";
 include "menuPAPOPE.php";
 ?>
 <form method="POST">
-Usuario: <input type="text" name="us_reg">
+Nombre: <input type="text" name="nom_reg">
+<br>
+Apellido: <input type="text" name="apel_reg">
+<br>
+Email (Obligatorio): <input type="text" name="em_reg">
 <br>
 Contraseña: <input type="password" name="pass_reg">
-Confirmar Contraseña: <input type="password" name="pass_reg_conf">
-
-
-
-
+<br>
+Telefono: <input type="text" name="tel_reg">
+<br>
+<input type="submit">
 </form>
+<?php
+$sql = "INSERT INTO usuarios VALUES (NULL, '".$_POST["nom_reg"]."', '".$_POST["apel_reg"]."', '".$_POST["em_reg"]."', '".$_POST["pass_reg"]."', '".$_POST["tel_reg"]."', NULL)";
 
+$res = consulta($conn, $sql);
+
+
+
+?>
 
