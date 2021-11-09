7c6006eaf5bef0be183792a7d195d92b1cfcd5b9
Fede <fedelva8@gmail.com>
2021-11-09 15:40:52 -0300

    WSDQWETFQ3WGIHGPIJORBIP

--- PHP/Usuario_listadoPAPOPE.php
+++ PHP/Usuario_listadoPAPOPE.php
@@ -14,7 +14,7 @@ $sql = "SELECT *
 		FROM usuarios 
 		WHERE nombre_usuario LIKE '%" . $_SESSION['nombre_usuario'] . "%' AND
 		apellido_usuario LIKE '%" . $_SESSION['apellido_usuario'] . "%' AND
-		(telefono_usuario LIKE '%" .$_SESSION['telefono_usuario']. "%' OR telefono_usuario IS NULL) AND
+		telefono_usuario LIKE '%" .$_SESSION['telefono_usuario']. "%' AND
 		email_usuario LIKE '%" .$_SESSION['email_usuario']. "%'";
 
 $res = consulta($conn, $sql);
@@ -24,10 +24,10 @@ $res = consulta($conn, $sql);
 <table border="1">
 	<tr>
 		<th>id</th>
+		<th>email</th>
 		<th>Nombre</th>
 		<th>apellido</th>
 		<th>telefono</th>
-		<th>email</th>
 		
 	</tr>
 	<?php while($fila = mysqli_fetch_assoc($res)){ ?>
