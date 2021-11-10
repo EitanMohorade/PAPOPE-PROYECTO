8e6a1441d05731cb821f98fe39931f51572d8477
Nicolas-Jaime <83721258+Nicolas-Jaime@users.noreply.github.com>
2021-11-10 12:06:31 -0300

    Add files via upload
    Agregado de la página "sacar turnos" (aún en proceso de completarse)

---
+++ PHP/sacarTurno.php
@@ -0,0 +1,13 @@
+<?
+
+    require_once(papopeconexion.php);
+
+php?>
+
+<form action="post">
+
+    Día: <input type="text"><br><br>
+    Horario: <input type="text"><br><br>
+    Club: <input type="text">
+
+</form>
