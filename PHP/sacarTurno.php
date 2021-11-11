49cebe18d25501a7582d2124e50df79b668ca2ab
Nicol4sEsquivel <laraqqqq53@gmail.com>
2021-11-11 14:21:16 -0300

    subí el archivo sacarTurno.php al repositorio

--- PHP/sacarTurno.php
+++ PHP/sacarTurno.php
@@ -1,13 +1,23 @@
-<?
+<?php
 
-    require_once(papopeconexion.php);
+    require_once("papopeconexion.php");
 
-php?>
+?>
 
 <form action="post">
 
     Día: <input type="text"><br><br>
     Horario: <input type="text"><br><br>
-    Club: <input type="text">
+    <select>
+    
+        <?php 
+        
+        $sql = "SELECT `nombre_cuenta` FROM `cuentas` WHERE tipo_id_cuenta = 2";
+        $res = mysqli_query($conn, $sql);
+        while($fila = mysqli_fetch_assoc($res)){
+            echo "<option>" . $fila['nombre_club'] . "</option>";
+        }?>
 
+    </select>
+    <input type="submit">
 </form>
