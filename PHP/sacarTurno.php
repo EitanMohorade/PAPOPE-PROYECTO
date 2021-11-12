4a7f10ac087fe713448c10d26e949474b2225639
Nicolas-Jaime <nicolas.jaime00@gmail.com>
2021-11-11 23:30:49 -0300

    lol

--- PHP/sacarTurno.php
+++ PHP/sacarTurno.php
@@ -28,7 +28,6 @@
 if(isset($_POST["dia"]) && isset($_POST["horario"]) && isset($_POST["club"])){
     $sql = "INSERT INTO turnos VALUES (null,". $_SESSION['id_cuenta'].",'" . $_POST['dia'] . "','" . $_POST['horario'] . "','" . $_POST['club'] . "', null)";
     $res = consulta($conn, $sql);
+    header('Location: listadoturnosPAPOPE.php');
 }
 ?>
-
- 
